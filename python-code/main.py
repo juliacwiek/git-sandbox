253a07a91e3c82a1b84c21beba5590eabd41437e
t.ferreria@live.ca <t.ferreira@live.ca>
2020-06-02 15:05:45 -0400

    testing...

--- python-code/main.py
+++ python-code/main.py
@@ -1,6 +1,11 @@
 def main():
     print("Test code")
     # --- ADD CODE HERE --- 
+    x = "julia"
+    def iscool(x):
+    	return True
+    if iscool(x):
+    	print(":)")
 
     
 if __name__ == "__main__":
